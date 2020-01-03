42a0108ce5e8c99bce0b27ff58e72f0f0c36200b
Estelle Choi <estele.choi@gmail.com>
2020-01-03 14:22:46 +0900

    Template Literals, fs 모듈, url 모듈

--- main.js
+++ main.js
@@ -2,49 +2,45 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); // url 모듈을 변수에 저장
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query; // query 정보를 담은 객체 반환
-    var title = queryData.id;
-    console.log(queryData.id);
+var app = http.createServer(function (request, response) {
+	var _url = request.url;
+	var queryData = url.parse(_url, true).query; // query 정보를 담은 객체 반환
+	var title = queryData.id;
 
-    if(_url == '/'){
-      title = "welcome";
-      _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
-    `;
+	if (_url == '/') {
+		title = "welcome";
+		_url = '/index.html';
+	}
+	if (_url == '/favicon.ico') {
+		return response.writeHead(404);
+	}
+	response.writeHead(200);
 
-
-    console.log(__dirname + url);
-    // fs.readFileSync : 경로에 해당하는 파일을 읽어서 가져온다.
-    // response.end(fs.readFileSync(__dirname + url));
-    response.end(template);
-
+	// 파일 읽기
+	fs.readFile(`data/${queryData.id}`, "UTF-8", function (err, data) {
+		var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${data}</p>
+      </body>
+      </html>
+      `;
+		// fs.readFileSync : 경로에 해당하는 파일을 읽어서 가져온다.
+		// response.end(fs.readFileSync(__dirname + url));
+		response.end(template);
+	});
 });
 // port: 3000
 // 웹서버 포트의 기본값은 80 (url 에서 생략 가능)

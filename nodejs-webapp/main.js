a1e4b99f20e95bf04e83cf799dbb4641684333e2
Estelle Choi <estele.choi@gmail.com>
2020-05-31 23:25:02 +0900

    app refactored using mysql to store data

--- nodejs-webapp/main.js
+++ nodejs-webapp/main.js
@@ -13,7 +13,7 @@ var sanitizeHtml = require("sanitize-html");
 var mysql = require("mysql");
 
 // connecting to mysql server
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
 	host: "localhost",
 	user: "temp_user",
 	password: "yk0425",
@@ -24,7 +24,7 @@ connection.connect();
 
 // * http.createServer(requestListener); http.Server 객체를 반환한다.
 // * The requestListener is a function which is automatically added to the 'request' event.
-// -> 웹어플리케이션 접속이 발새할 때마다 createServer의 콜백함수가 호출된다.
+// -> 웹어플리케이션 접속시마다 createServer의 콜백함수가 호출된다.
 var app = http.createServer(function (request, response) {
 	var _url = request.url; // 사용자 요청에서 url을 반환
 	console.log(url.parse(_url, true)); // 해당 url을 파싱하여 객체를 반환
@@ -33,33 +33,13 @@ var app = http.createServer(function (request, response) {
 	var pathname = url.parse(_url, true).pathname; // queryString 제외한 path 반환
 
 	if (pathname === "/") {
-		// queryString 이 없을 때 (홈 화면)
 		if (queryData.id === undefined) {
-			// fs.readdir(); 폴더의 파일 목록을 가져오기 (파일명) - 비동기 메소드이다.
-			// fs.readdir("./data", function (err, fileList) {
-			// 	var title = "Welcome";
-			// 	var data = "Hello node.js";
-			// 	var list = template.getList(fileList);
-			// 	var html = template.getHTML(
-			// 		title,
-			// 		list,
-			// 		`<h2>${title}</h2><p>${data}</p>`,
-			// 		`<a href="/create">create</a>`
-			// 	);
-			// 	response.writeHead(200); // 응답코드
-			// 	response.end(html); // template 을 응답
-			// });
-			connection.query(`SELECT * FROM topic`, function (err, results) {
-				if (err) {
-					console.log(err);
-					throw err;
-				}
+			connection.query(`SELECT * FROM topic`, (err, results) => {
+				if (err) throw err; // if error occurs, console prints the error and this app stops.
 
 				const title = "Welcome";
 				const data = "Hello node.js";
-
-				// results is an array of objects.
-				const list = template.getList(results);
+				const list = template.getList(results); // results is an array of objects.
 				const html = template.getHTML(
 					title,
 					list,
@@ -69,52 +49,51 @@ var app = http.createServer(function (request, response) {
 				response.writeHead(200);
 				response.end(html);
 			});
-		}
-		// queryString 있을 때
-		else {
-			fs.readdir("./data", function (err, fileList) {
-				// *** 입력 관련 보안장치 (사용자가 중요파일에 접근하지 못하도록)
-				// * path.parse(path); URL(요청경로)을 파싱하여 경로정보를 담은 객체 반환
-				// * base 속성의 값: 경로를 제외한 파일명
-				// ../ 와 같은 디렉토리 탐색이 가능한 요청을 제거하도록 필터링
-				var idFiltered = path.parse(queryData.id).base;
+		} else {
+			connection.query(`SELECT * FROM topic`, (err, topics) => {
+				if (err) throw err;
+				// using ? in sql query blocks possible hacking attempts.
+				connection.query(
+					`SELECT * FROM topic WHERE id = ?`,
+					[queryData.id],
+					(err, results) => {
+						if (err) throw err;
+						// console.log(results);
 
-				// fs.readFile(); 파일 내용 읽어오기
-				fs.readFile(`data/${idFiltered}`, "UTF-8", function (err, data) {
-					var title = queryData.id;
-					var list = template.getList(fileList);
+						if (results.length) {
+							const res = results[0];
+							const title = res.title;
+							const data = res.description;
 
-					// *** 출력 관련 보안장치 (위험한 입력값이 html 태그로서 동작하지 못하도록 ex) <script>)
-					// * sanitize-html 모듈을 사용하여 html태그를 없앤 문자열을 반환한다.
-					// 파라미터를 사용하여 허용되는 html 태그를 설정할 수 있다.
-					var titleSani = sanitizeHtml(title);
-					var dataSani = sanitizeHtml(data, {
-						allowedTags: ["h1"],
-					});
-
-					// delete 기능은 보안상의 이유로 GET 방식으로 하면 안된다. (삭제 URL이 외부에 노출되면 누구나 삭제가 가능할 수 있기 때문)
-					var html = template.getHTML(
-						titleSani,
-						list,
-						`<h2>${titleSani}</h2><p>${dataSani}</p>`,
-						`<a href="/create">create</a>
-						 <a href="/update?id=${qs.escape(titleSani)}">update</a>
-						 <form action="/delete_process" method="post">
-						 	<input type="hidden" name="id" value="${titleSani}">
-							<input type="submit" value="delete">
-						 </form>`
-					);
-					// response.end(fs.readFileSync(__dirname + url));
-					response.writeHead(200);
-					response.end(html);
-				});
+							const list = template.getList(topics);
+							const html = template.getHTML(
+								title,
+								list,
+								`<h2>${title}</h2><p>${data}</p>`,
+								`<a href="/create">create</a>
+							 <a href="/update?id=${queryData.id}">update</a>
+							 <form action="/delete_process" method="post">
+								 <input type="hidden" name="id" value="${queryData.id}">
+								<input type="submit" value="delete">
+							 </form>`
+							);
+							response.writeHead(200);
+							response.end(html);
+							return;
+						}
+						response.writeHead(200);
+					}
+				);
 			});
 		}
 	} else if (pathname === "/create") {
-		fs.readdir("./data", function (err, fileList) {
-			var title = "Create";
-			var list = template.getList(fileList);
-			var html = template.getHTML(
+		connection.query(`SELECT * FROM topic`, (err, topics) => {
+			if (err) throw err;
+
+			const title = "Create";
+			console.log(title);
+			const list = template.getList(topics);
+			const html = template.getHTML(
 				title,
 				list,
 				`
@@ -123,122 +102,132 @@ var app = http.createServer(function (request, response) {
 					<textarea name="description" placeholder="description"></textarea><br>
 					<input type="submit" value="ok"><br>
 				</form>
-			`,
-				""
+				`,
+				title
 			);
 			response.writeHead(200); // 응답코드
 			response.end(html); // template 을 응답
 		});
 	} else if (pathname === "/create_process") {
-		// ** 각 이벤트 발생시 호출될 콜백함수를 작성
 		// * POST 전송 데이터를 수신하는 이벤트
 		// POST 전송 데이터가 매우 많은 경우를 대비하여 데이터의 일부를 수신할 때마다 콜백함수를 호출하여 데이터를 저장한다.
-		var body = "";
-		request.on("data", function (data) {
+		let body = "";
+		request.on("data", (data) => {
 			body += data;
-			// for too much POST data, kill the connection. (보안상)
+			// if too much POST data, kill the connection. (for security)
 			// 1e6 : Math.pow(10, 6) === 1 * 1000000 ~ 1MB
 			if (body.length > 1e6) {
 				request.connection.destroy();
 			}
 		});
 
-		// * 더이상 수신되는 데이터가 없는 이벤트
-		request.on("end", function () {
-			// querystring 모듈의 메소드 사용
-			// 수신한 POST 데이터를 담은 객체를 반환
-			var postData = qs.parse(body);
+		// on end of sending request data
+		request.on("end", () => {
+			// 수신한 POST 데이터 객체
+			const postData = qs.parse(body);
 			console.log(postData);
-			var title = postData.title; // encodeURI vs qs.escape() ?
-			var titleFiltered = path.parse(title).base;
-			var description = postData.description;
 
-			// * fs.writeFile(파일 경로, 파일 내용, 인코딩, 콜백함수); 파일을 작성한다. (파일 경로가 없다면 새 파일 생성)
-			fs.writeFile(`data/${titleFiltered}`, description, "UTF-8", function (
-				err
-			) {
-				// 콜백함수의 결과에 따라 수행될 내용은 비동기 메소드 안에 작성해야 ..
-				// * redirect 코드 : 302
-				// * qs.escape(); qs.unescape();
-				// URL은 아스키코드로 이뤄져야하기 때문에 그 외의 문자는 “%”와 16진수 문자를 조합해 인코딩해야 한다. (escapte 처리)
-				response.writeHead(302, {
-					Location: `/?id=${qs.escape(titleFiltered)}`,
-				});
-				response.end();
-			});
-		});
-	} else if (pathname === "/update") {
-		fs.readdir("./data", function (err, fileList) {
-			var idFiltered = path.parse(queryData.id).base;
-			fs.readFile(`data/${idFiltered}`, "UTF-8", function (err, data) {
-				var title = queryData.id;
-				var list = template.getList(fileList);
-				var html = template.getHTML(
-					title,
-					list,
-					`
-					<form action="/update_process" method="post">
-						<input type="hidden" name="id" value="${title}">
-						<input type="text" name="title" placeholder="title" value="${title}"><br>
-						<textarea name="description" placeholder="description">${data}</textarea><br>
-						<input type="submit" value="ok"><br>
-					</form>
-				`,
-					""
-				);
-				response.writeHead(200); // 응답코드
-				response.end(html); // template 을 응답
-			});
-		});
-	} else if (pathname === "/update_process") {
-		var body = "";
-		request.on("data", function (data) {
-			body += data;
-		});
-		request.on("end", function () {
-			// 수신한 POST 데이터를 담은 객체를 반환
-			var postData = qs.parse(body);
-			var id = postData.id;
-			var title = postData.title;
-			var description = postData.description;
-			var idFiltered = path.parse(id).base;
-			var titleFiltered = path.parse(title).base;
-			console.log(postData);
-			// * fs.rename(old path, new path, callback); 파일명을 바꾼다.
-			fs.rename(`data/${idFiltered}`, `data/${titleFiltered}`, function (err) {
-				// * fs.writeFile(); 파일을 작성한다.
-				fs.writeFile(`data/${titleFiltered}`, description, "UTF-8", function (
-					err
-				) {
-					// * redirect 코드 : 302
+			const title = postData.title; // encodeURI vs qs.escape() ?
+			const titleFiltered = path.parse(title).base;
+			const description = postData.description;
+			connection.query(
+				`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`,
+				[titleFiltered, description, 1],
+				(err, results) => {
+					if (err) throw err;
+
 					response.writeHead(302, {
-						Location: `/?id=${qs.escape(titleFiltered)}`,
+						Location: `/?id=${results.insertId}`,
 					});
 					response.end();
-				});
-			});
+				}
+			);
 		});
-	} else if (pathname === "/delete_process") {
-		var body = "";
+	} else if (pathname === "/update") {
+		connection.query(`SELECT * FROM topic`, (err, topics) => {
+			if (err) throw err;
+
+			const idFiltered = path.parse(queryData.id).base;
+			connection.query(
+				`SELECT * FROM topic WHERE id = ?`,
+				[idFiltered],
+				(error, results) => {
+					if (error) throw error;
+
+					if (results.length) {
+						const res = results[0];
+						const title = res.title;
+						const data = res.description;
+						const list = template.getList(topics);
+						const html = template.getHTML(
+							title,
+							list,
+							`
+							<form action="/update_process" method="post">
+								<input type="hidden" name="id" value="${res.id}">
+								<input type="text" name="title" placeholder="title" value="${title}"><br>
+								<textarea name="description" placeholder="description">${data}</textarea><br>
+								<input type="submit" value="ok"><br>
+							</form>
+							`,
+							"Update"
+						);
+						response.writeHead(200); // 응답코드
+						response.end(html); // template 을 응답
+						return;
+					}
+					response.writeHead(200); // 응답코드
+				}
+			);
+		});
+	} else if (pathname === "/update_process") {
+		let body = "";
 		request.on("data", function (data) {
 			body += data;
 		});
 		request.on("end", function () {
-			var postData = qs.parse(body);
-			var id = postData.id;
-			var idFiltered = path.parse(id).base; // 요청 id 필터링 (보안처리)
+			// 수신한 POST 데이터를 담은 객체를 반환
+			const postData = qs.parse(body);
+			const id = postData.id;
+			const title = postData.title;
+			const description = postData.description;
+			const idFiltered = path.parse(id).base;
+			const titleFiltered = path.parse(title).base;
 
-			// fs.unlink(); 파일 또는 symbolic link를 삭제한다.
-			fs.unlink(`data/${idFiltered}`, function (err) {
-				response.writeHead(302, {
-					Location: `/`,
-				});
-				response.end();
-			});
+			connection.query(
+				`UPDATE topic SET title = ?, description = ? WHERE id = ?`,
+				[titleFiltered, description, idFiltered],
+				(err) => {
+					if (err) throw err;
+
+					response.writeHead(302, {
+						Location: `/?id=${idFiltered}`,
+					});
+					response.end();
+				}
+			);
 		});
-	}
-	// pathname !== "/" (경로 오류)
-	else {
+	} else if (pathname === "/delete_process") {
+		let body = "";
+		request.on("data", function (data) {
+			body += data;
+		});
+		request.on("end", function () {
+			const postData = qs.parse(body);
+			const id = postData.id;
+			const idFiltered = path.parse(id).base; // 요청 id 필터링 (보안처리)
+
+			connection.query(
+				`DELETE FROM topic WHERE id = ?`,
+				[idFiltered],
+				(err) => {
+					if (err) throw err;
+					response.writeHead(302, { Location: `/` });
+					response.end();
+				}
+			);
+		});
+	} else {
 		response.writeHead(404);
 		response.end("Not found");
 	}

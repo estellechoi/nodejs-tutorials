f35936692b318e08f357863727398f5da9040c4f
Estelle Choi <estele.choi@gmail.com>
2020-06-12 17:11:48 +0900

    mysql connection

--- nodejs-webapp/lib/db.js
+++ nodejs-webapp/lib/db.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 
 // connecting to mysql server
+// this is just to show original format, the user, password and other db connection information should be seperated and git-ignored for security.
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "temp_user",

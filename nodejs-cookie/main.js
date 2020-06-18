6bac1e709ae8044448f671735a3216a27005a27b
Estelle Choi <estele.choi@gmail.com>
2020-06-18 22:58:02 +0900

    set session cookie, permanent cookie and get cookies

--- nodejs-cookie/main.js
+++ nodejs-cookie/main.js
@@ -3,16 +3,20 @@ const cookie = require("cookie");
 
 http
 	.createServer(function (req, res) {
-		let cookie = {};
+		let cookies = {};
 
 		// if (req.headers.cookie !== undefined)
-		if (req.headers.cookie) cookie = cookie.parse(req.headers.cookie); // parse string to object.
+		if (req.headers.cookie) cookies = cookie.parse(req.headers.cookie); // parse string to object.
 
-		console.log(cookie.cookie1); // output : choco
+		console.log(cookies.cookie1); // output : choco
 
 		// set cookie
 		res.writeHead(200, {
-			"Set-Cookie": ["cookie1=choco", "cookie2=strawberry"],
+			"Set-Cookie": [
+				"cookie1=choco", // session cookie
+				"cookie2=strawberry",
+				`Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`, // permanent cookie with max valid number of seconds
+			],
 		});
 		res.end("Cookie Test");
 	})
